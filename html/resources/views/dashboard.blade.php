ac847503ed45754dd7a2906c74431365e0a80aeb
A3C2112 <anderson_2192@yahoo.com>
2023-06-30 12:07:42 +0200

    AdminLte

---
+++ html/resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte::page')
+
+@section('content_header')
+    <h1>Inco Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
