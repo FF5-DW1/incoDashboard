b5adf3a73e8fd6696ff711525aebc4e6ff424b15
gladys@gmail.com <bea@hotmail.com>
2023-07-27 15:41:29 +0200

    commit de test

--- html/tests/Feature/UserTest.php
+++ html/tests/Feature/UserTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use App\Models\User;
 class UserTest extends TestCase
@@ -11,44 +13,39 @@ class UserTest extends TestCase
     /**
      * A basic feature test example.
      */
-    use RefreshDatabase;
+    
+
+     use DatabaseTransactions;
+     use WithoutMiddleware;
+
     public function test_verificar_si_existe_usuario_basedatos(): void
     {
         //User::factory()->count(2)->create();
+        User::factory()->create(['email' => 'angel@Inco.com']);
 
-        // Verificar si la tabla 'users' tiene exactamente dos registros
-        $this->assertDatabaseCount('users', 2);
+        $this->assertDatabaseCount('users', 3);
 
-        // Verificar si la tabla 'users' tiene un registro con el email especificado
-        $this->assertDatabaseHas('users', ['email' => 'angel@Inco.com']);
+    
 
-        // Realizar una solicitud de prueba a la ruta '/login' (puedes cambiarlo por la ruta que corresponda a tu login)
-        $response = $this->get('/login');
-
-        // Verificar si la respuesta tiene un código de estado HTTP 200
-        $response->assertStatus(200);
+      $this->assertDatabaseHas('users', ['email' => 'angel@Inco.com']);
+    
+    }
+    
+    public function test_verificar_si_usuario_puede_ver_dashboard(): void
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)
+                         ->get('/app/dashboard');   
+                         $response->assertStatus(200);
     
     }
 
-    // public function test_admin_puede_entrar_a_dashboard()
-    // {
-    //     // Crear un usuario administrador en la base de datos (asegúrate de tener una columna 'role' que indique si es administrador)
-    //     $adminUser = Users::factory()->create(['role' => 'Administrador']);
-
-    //     // Iniciar sesión como el usuario administrador
-    //     $this->actingAs($adminUser);
-
-    //     // Realizar la solicitud GET a la ruta de la dashboard
-    //     $response = $this->get(route('dashboard'));
-
-    //     // Verificar que la solicitud fue exitosa (status code 200)
-    //     $response->assertStatus(200);
-
-    //     // Verificar que el usuario administrador está viendo la vista correcta
-    //     $response->assertViewIs('dashboard'); // Cambia 'dashboard' por el nombre de la vista de la dashboard
-    // }
-    //$response = $this->assertDatabaseCount('users',['email' => 'angel@Inco.com',]);
+    public function test_verificar_si_usuario_puede_ver_graficos(): void
+    {
+        $user = User::factory()->make();
+        $response = $this->actingAs($user)
+                         ->get('/app/charts');   
+                         $response->assertStatus(200);
     
-
-    //$this->assertDatabaseCount('users',2);
+    }
 }
