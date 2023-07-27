c8f6a5d907a5b0b9b6cc57a6258033dc0226a939
JesCab29 <jesusacs2906@gmail.com>
2023-07-27 16:53:12 +0200

    añadido favicon de inco en todas las views

--- html/resources/views/layout/home.blade.php
+++ html/resources/views/layout/home.blade.php
@@ -11,6 +11,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <!-- Estilos -->
+    <link rel="icon" type="image/png" href="{{ asset('/images/faviconV2.png') }}">
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}" class="rel">
     <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
     @yield('css')
